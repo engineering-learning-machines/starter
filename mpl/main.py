00e046f392bc879940aa2f51849135467d5be988
g6714 <venelin.petkov@man.eu>
2018-04-02 05:49:23 +0200

    Preprocessed data to convert to proper format before training

--- mpl/main.py
+++ mpl/main.py
@@ -211,28 +211,37 @@ if __name__ == '__main__':
     log.info('Test data set: {} images'.format(test_labels.shape[0]))
     log.info('Loaded in {:.1f} seconds'.format(time.time() - start))
 
-    # TODO:
-    # Convert the data into the proper format while loading, should be faster
-    # because of numpy
+    # Convert the data into the proper format to speed up calculations
+    start = time.time()
+    log.info('Converting data...')
+    # Convert the images to image vectors
+    image_vector_length = training_images.shape[1] * training_images.shape[2]
+    training_images.shape = (training_images.shape[0], image_vector_length)
+    test_images.shape = (test_images.shape[0], image_vector_length)
+    # One-hot encode the labels
+    training_labels = [encode_one_hot(label) for label in training_labels]
+    test_labels = [encode_one_hot(label) for label in test_labels]
+    log.info('Done in {:.1f} s'.format(time.time() - start))
 
     # Prepare for testing
-    test_index = list(range(test_labels.shape[0]))
+    test_index = list(range(test_images.shape[0]))
     # Train the system
     start_total_training_time = time.time()
-    for batch_index, batch_partition in enumerate(range(test_labels.shape[0])[::BATCH_SIZE]):
+    for batch_index, batch_partition in enumerate(range(test_images.shape[0])[::BATCH_SIZE]):
         if batch_index >= MAX_BATCH_COUNT:
             break
         # Process one batch
         start_batch = time.time()
         batch_data = test_images[batch_partition:batch_partition+BATCH_SIZE]
         batch_labels = test_labels[batch_partition:batch_partition+BATCH_SIZE]
-        for image, label_digit in zip(batch_data, batch_labels):
-            image.shape = (image.shape[0] * image.shape[1],)
-            label = encode_one_hot(label_digit)
+        #for image, label_digit in zip(batch_data, batch_labels):
+        for image, label in zip(batch_data, batch_labels):
+            # image.shape = (image.shape[0] * image.shape[1],)
+            # label = encode_one_hot(label_digit)
             net.evaluate(image)
             net.backpropagate(label)
         # Update weights when finished with batch
-        net.update_weights(batch_labels.shape[0], LEARN_RATE)
+        net.update_weights(batch_data.shape[0], LEARN_RATE)
         log.debug('Processed batch: {:0>6} [{:.1f}] s'.format(batch_index+1, time.time() - start_batch))
 
         # Test every BATCH_TEST_INTERVAL batches:
@@ -240,13 +249,18 @@ if __name__ == '__main__':
         if (batch_index + 1) % BATCH_TEST_INTERVAL == 0:
             total_loss = 0
             shuffle(test_index)
-            for image, label_digit in zip(
-                    test_images[test_index[:BATCH_SIZE]], test_labels[test_index[:BATCH_SIZE]]
-            ):
-              image.shape = (image.shape[0] * image.shape[1],)
-              label = encode_one_hot(label_digit)
-              net.evaluate(image)
-              total_loss += net.get_output_squared_error(label)
+            # for image, label_digit in zip(
+            #for image, label in zip(
+            #        test_images[test_index[:BATCH_SIZE]],
+            #        test_labels[test_index[:BATCH_SIZE]]
+            #):
+            for index in test_index[:BATCH_SIZE]:
+              # image.shape = (image.shape[0] * image.shape[1],)
+              # label = encode_one_hot(label_digit)
+                image = test_images[index]
+                label = test_labels[index]
+                net.evaluate(image)
+                total_loss += net.get_output_squared_error(label)
             total_loss = total_loss / BATCH_SIZE
             log.info('Test Loss: {:.6f}'.format(total_loss))
 
